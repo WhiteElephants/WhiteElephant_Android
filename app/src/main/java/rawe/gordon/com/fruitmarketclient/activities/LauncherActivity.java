02a8bee62a9235f5ba0ae36b9d8a7acf8e7091ea
gordon <gordon@Jason-Air.local>
2016-08-03 22:32:29 +0800

    soemthing

--- app/src/main/java/rawe/gordon/com/fruitmarketclient/activities/LauncherActivity.java
+++ app/src/main/java/rawe/gordon/com/fruitmarketclient/activities/LauncherActivity.java
@@ -30,7 +30,7 @@ public class LauncherActivity extends AppCompatActivity {
         tabLayout.configure(new LauncherTabLayout.Tab[]{
                 new LauncherTabLayout.Tab(R.drawable.ic_credit_card_orange, "首页", R.drawable.ic_credit_card_grey),
                 new LauncherTabLayout.Tab(R.drawable.ic_credit_card_orange, "发现", R.drawable.ic_credit_card_grey),
-                new LauncherTabLayout.Tab(R.drawable.ic_camera, "", R.drawable.ic_camera),
+                new LauncherTabLayout.Tab(R.drawable.ic_camera_gray, "", R.drawable.ic_camera_gray),
                 new LauncherTabLayout.Tab(R.drawable.ic_settings_orange, "创作", R.drawable.ic_settings_grey),
                 new LauncherTabLayout.Tab(R.drawable.ic_person_orange, "个人", R.drawable.ic_person_gray)
         }, 0).hookUpWithViewPager(fragmentPager);
